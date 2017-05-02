af366874c0bdea4d0211046d0caa69888479b81e
Cassie Musolf and Jiwon Kang <cassie+jiwon@>
2017-05-02 10:11:28 -0700

    follow homework for ajax demos.

---
+++ src/AjaxDemo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AjaxDemo.Models;
+
+namespace AjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AjaxDemoContext db = new AjaxDemoContext();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RandomDestinationList(int destinationCount)
+        {
+            var randomDestinationList = db.Destinations.OrderBy(r => Guid.NewGuid()).Take(destinationCount);
+            return Json(randomDestinationList);
+        }
+
+        [HttpPost]
+        public IActionResult NewDestination(string newCity, string newCountry)
+        {
+            Destination newDestination = new Destination(newCity, newCountry);
+            db.Destinations.Add(newDestination);
+            db.SaveChanges();
+            return Json(newDestination);
+        }
+    }
+}
